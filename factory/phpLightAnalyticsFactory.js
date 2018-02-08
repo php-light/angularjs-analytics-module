d7ca16822152e8746ccced75b67228daee6740d7
Khalid Sookia <khalidsookia@gmail.com>
2018-02-08 21:31:25 +0100

    added ignore paths for stats

--- factory/phpLightAnalyticsFactory.js
+++ factory/phpLightAnalyticsFactory.js
@@ -7,19 +7,28 @@
 
 'use strict';
 
-phpLightCommentModule.factory('phpLightAnalyticsFactory', ['$rootScope', '$http', '$q', '$window',
-    function ($rootScope, $http, $q, $window) {
+phpLightCommentModule.factory('phpLightAnalyticsFactory', ['$rootScope', '$http', '$q', '$window', 'VALUE_PARAMETERS',
+    function ($rootScope, $http, $q, $window, VALUE_PARAMETERS) {
         var phpLightAnalyticsFactory = {};
 
         phpLightAnalyticsFactory.send = function (data) {
-            var deferred = $q.defer();
-
+            var shouldIgnore = false;
             data.currentUrl = $window.location.href;
             data.currentHash = $window.location.hash;
 
+            angular.forEach(VALUE_PARAMETERS.phpLightAnalytics.ignore.hash.startsWith, function (ignore) {
+                if (data.currentHash.match(new RegExp('^#!\\/' + ignore , 'i'))) {
+                    shouldIgnore = true;
+                }
+            });
+
+            if (shouldIgnore) return;
+
+            var deferred = $q.defer();
+
             $http.post($rootScope.restUrl + "?route=php_light_analytics_send", { data: data }).then(
                 function (response) {
-                    deferred.resolve(response);
+                    deferred.resolve(response.data);
                 },
                 function (error) {
                     console.error('Error in phpLightAnalyticsFactory send method');
